2cf041371d7bec69ac070182a50532b897735fef
pabgonmon2 <pablogonmon@gmail.com>
2021-04-10 12:20:34 +0200

    Commit

--- src/main/java/acme/features/anonymous/shout/AnonymousShoutRepository.java
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutRepository.java
@@ -9,7 +9,7 @@ import acme.entities.shouts.Shout;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
-public interface AnonymousShoutRepository extends AbstractRepository{
+public interface AnonymousShoutRepository extends AbstractRepository {
 	
 	@Query("select s from Shout s")
 	Collection<Shout> findMany();
