ee5a0a30ceba161bb60cabe608f3e64070849d25
JoseCarlosMorales <josmorbor3@alum.us.es>
2021-04-12 13:38:56 +0200

    Tarea 5:listing-tasks

---
+++ src/main/java/acme/features/anonymous/task/AnonymousTaskRepository.java
@@ -0,0 +1,16 @@
+package acme.features.anonymous.task;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.tasks.Task;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AnonymousTaskRepository extends AbstractRepository{
+	
+	@Query("select t from Task t")
+	Collection<Task> findMany();
+}
