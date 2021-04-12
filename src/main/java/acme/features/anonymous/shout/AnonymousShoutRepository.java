8d3824f2f600842aa8f1871b7956b91e1a2daa9f
pabgonmon2 <pablogonmon@gmail.com>
2021-04-12 14:55:56 +0200

    Listar shouts del último mes para anonymous.

--- src/main/java/acme/features/anonymous/shout/AnonymousShoutRepository.java
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutRepository.java
@@ -1,6 +1,7 @@
 package acme.features.anonymous.shout;
 
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,5 +14,8 @@ public interface AnonymousShoutRepository extends AbstractRepository{
 	
 	@Query("select s from Shout s")
 	Collection<Shout> findMany();
+	
+	@Query("select s from Shout s where s.moment >= ?1")
+	Collection<Shout> findRecentShouts(Date deadline);
 
 }
