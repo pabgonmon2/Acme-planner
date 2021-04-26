b6306931718bb29dd196c63f5add1418b2651fc2
Carlos Villadiego <carvilgar1@alum.us.es>
2021-04-26 17:41:03 +0200

    Ahora cualquier usuario que se registre es Manager

--- src/main/java/acme/features/authenticated/manager/AuthenticatedManagerCreateService.java
+++ src/main/java/acme/features/authenticated/manager/AuthenticatedManagerCreateService.java
@@ -10,7 +10,7 @@
  * they accept any liabilities with respect to them.
  */
 
-package acme.features.authenticated.consumer;
+package acme.features.authenticated.manager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
--- src/main/java/acme/features/authenticated/manager/AuthenticatedManagerRepository.java
+++ src/main/java/acme/features/authenticated/manager/AuthenticatedManagerRepository.java
@@ -10,7 +10,7 @@
  * they accept any liabilities with respect to them.
  */
 
-package acme.features.authenticated.consumer;
+package acme.features.authenticated.manager;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
