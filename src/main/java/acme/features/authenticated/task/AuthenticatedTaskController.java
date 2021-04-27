414b34471743e4eee853a148f6d7c79dc3b034c7
JoseCarlosMorales <josmorbor3@alum.us.es>
2021-04-27 22:06:26 +0200

    Authenticated show task

--- src/main/java/acme/features/authenticated/task/AuthenticatedTaskController.java
+++ src/main/java/acme/features/authenticated/task/AuthenticatedTaskController.java
@@ -18,10 +18,14 @@ public class AuthenticatedTaskController extends AbstractController<Authenticate
 	@Autowired 
 	private AuthenticatedTaskListService listService;
 	
+	@Autowired
+	private AuthenticatedTaskShowService showService;
+	
 	
 	@PostConstruct
 	private void initialise() {
 		super.addBasicCommand(BasicCommand.LIST, this.listService);
+		super.addBasicCommand(BasicCommand.SHOW, this.showService);
 	}
 	
 }
