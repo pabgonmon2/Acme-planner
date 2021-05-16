bf123005952aeec295755d5ab0488050bc34cd7c
Carlos Villadiego <carvilgar1@alum.us.es>
2021-05-16 17:40:12 +0200

    TEST 004 FINISHED
    Administrator WorkPlan DashBoard Test Case

---
+++ src/test/java/acme/testing/administrator/workplan/AdministratorWorkPlanDashBoardTestCase.java
@@ -0,0 +1,63 @@
+package acme.testing.administrator.workplan;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AdministratorWorkPlanDashBoardTestCase extends AcmePlannerTest{
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/administrator/workplan/workplan-dashboard.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void listAll(final int recordIndex, 
+	final String publicTasks,
+	final String privateTasks,
+	final String finishedTasks,
+	final String nonFinishedTasks,
+	final String averageWorkFlow,
+	final String deviationWorkFlow,
+	final String maxWorkFlow,
+	final String minWorkFlow,
+	final String averageExecutionPeriod,
+	final String deviationExecutionPeriod,
+	final String maxExecutionPeriod,
+	final String minExecutionPeriod) {
+		
+		this.signIn("administrator", "administrator");
+		super.clickOnMenu("Administrator", "Dashboard Work Plans");
+		
+		super.checkColumnHasValue(recordIndex, 0, publicTasks);
+		super.checkColumnHasValue(recordIndex, 1, privateTasks);
+		super.checkColumnHasValue(recordIndex, 2, finishedTasks);
+		super.checkColumnHasValue(recordIndex, 3, nonFinishedTasks);
+		super.checkColumnHasValue(recordIndex, 4, averageWorkFlow);
+		super.checkColumnHasValue(recordIndex, 5, deviationWorkFlow);
+		super.checkColumnHasValue(recordIndex, 6, maxWorkFlow);
+		super.checkColumnHasValue(recordIndex, 7, minWorkFlow);
+		super.checkColumnHasValue(recordIndex, 8, averageExecutionPeriod);
+		super.checkColumnHasValue(recordIndex, 9, deviationExecutionPeriod);
+		super.checkColumnHasValue(recordIndex, 10, maxExecutionPeriod);
+		super.checkColumnHasValue(recordIndex, 11, minExecutionPeriod);
+		
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("publicTasks", publicTasks);
+		super.checkInputBoxHasValue("privateTasks", privateTasks);
+		super.checkInputBoxHasValue("finishedTasks", finishedTasks);
+		super.checkInputBoxHasValue("nonFinishedTasks", nonFinishedTasks);
+		super.checkInputBoxHasValue("averageWorkFlow", averageWorkFlow);
+		super.checkInputBoxHasValue("deviationWorkFlow", deviationWorkFlow);
+		super.checkInputBoxHasValue("maxWorkFlow", maxWorkFlow);
+		super.checkInputBoxHasValue("minWorkFlow", minWorkFlow);
+		super.checkInputBoxHasValue("averageExecutionPeriod", averageExecutionPeriod);
+		super.checkInputBoxHasValue("deviationExecutionPeriod", deviationExecutionPeriod);
+		super.checkInputBoxHasValue("maxExecutionPeriod", maxExecutionPeriod);
+		super.checkInputBoxHasValue("minExecutionPeriod", minExecutionPeriod);
+		
+		this.signOut();
+	}
+
+
+}
