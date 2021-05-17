ddceb7c6a69978675af17876c12582a9c8107132
pabgonmon2 <pablogonmon@gmail.com>
2021-05-17 17:38:01 +0200

    Incremented percentage of ListSpamword

--- src/test/java/acme/testing/administrator/spamfilter/spamword/AdministratorSpamwordListTest.java
+++ src/test/java/acme/testing/administrator/spamfilter/spamword/AdministratorSpamwordListTest.java
@@ -43,4 +43,84 @@ public class AdministratorSpamwordListTest extends AcmePlannerTest{
 		super.navigate("/administrator/spamword/list", ""); ;
 		super.checkErrorsExist();
 	}
+	
+	@Test
+	@Order(35)
+	public void filtrosShoutPositive() {
+		
+		super.clickOnMenu("Anonymous", "Shouts!");
+		
+		super.checkInputBoxHasValue("author", "John Doe");
+		super.checkInputBoxHasValue("text", "Lorem ipsum!");
+		super.checkInputBoxHasValue("info", "http://example.org");
+		
+		super.fillInputBoxIn("author", "Pablo");
+		super.fillInputBoxIn("text", "Hola como estais?");
+		super.fillInputBoxIn("info", "");
+		
+		super.clickOnSubmitButton("Shout!");
+		
+		super.clickOnMenu("Anonymous", "List shout");
+		
+		super.checkColumnHasValue(0, 1, "Pablo");
+		super.checkColumnHasValue(0, 2, "Hola como estais?");
+		super.checkColumnHasValue(0, 3, "");
+		
+	}
+	
+	@Test
+	@Order(40)
+	public void filtrosShoutNegative() {
+		
+		super.clickOnMenu("Anonymous", "Shouts!");
+		
+		super.checkInputBoxHasValue("author", "John Doe");
+		super.checkInputBoxHasValue("text", "Lorem ipsum!");
+		super.checkInputBoxHasValue("info", "http://example.org");
+		
+		super.fillInputBoxIn("author", "Sex");
+		super.fillInputBoxIn("text", "Hola como estais?");
+		super.fillInputBoxIn("info", "");
+		
+		super.clickOnSubmitButton("Shout!");
+		
+		super.checkErrorsExist();
+		
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/task/createPositive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(50)
+	public void createPositive(final int recordIndex, final String title, final String startDate, 
+		final String endDate, final String workFlow, final String description, final String publicTask, final String url) {
+		super.signIn("manager3", "manager3");
+		
+		super.clickOnMenu("Manager", "Create task");
+		
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("startDate", startDate);
+		super.fillInputBoxIn("endDate", endDate);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("url", url);
+		super.clickOnSubmitButton("Create task!");
+		
+		super.clickOnMenu("Manager", "Own tasks");
+		
+		super.checkColumnHasValue(recordIndex, 0, title);
+		super.checkColumnHasValue(recordIndex, 1, startDate);
+		super.checkColumnHasValue(recordIndex, 2, endDate);
+		super.checkColumnHasValue(recordIndex, 3, description);
+		
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("startDate", startDate);
+		super.checkInputBoxHasValue("endDate", endDate);
+		super.checkInputBoxHasValue("workFlow", workFlow);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("publicTask", publicTask);
+		super.checkInputBoxHasValue("url", url);
+		
+		super.signOut();
+	}
 }
--- src/test/java/acme/testing/administrator/spamfilter/threshold/AdministratorThresholdUpdateTest.java
+++ src/test/java/acme/testing/administrator/spamfilter/threshold/AdministratorThresholdUpdateTest.java
@@ -26,6 +26,7 @@ public class AdministratorThresholdUpdateTest extends AcmePlannerTest{
 		super.checkInputBoxHasValue("value", value);
 		
 		super.fillInputBoxIn("value", "15.00");
+
 		super.clickOnSubmitButton("Update");
 		
 		super.clickOnMenu("Administrator", "Spam threshold");
@@ -61,4 +62,27 @@ public class AdministratorThresholdUpdateTest extends AcmePlannerTest{
 		super.checkErrorsExist("value");
 
 	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/administrator/spamfilter/listThreshold.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void returnTest(final int recordIndex, final String value) {
+		this.signIn("administrator", "administrator");
+		
+		super.clickOnMenu("Administrator", "Spam threshold");
+		
+		super.checkColumnHasValue(recordIndex, 0, value);
+		
+		super.clickOnListingRecord(recordIndex);
+		
+//		super.checkSimplePath("/administrator/threshold/show");
+		
+		super.checkInputBoxHasValue("value", value);
+		super.clickOnReturnButton("Return");
+		super.checkColumnHasValue(recordIndex, 0, value);
+		
+
+	}
+
+	
 }
