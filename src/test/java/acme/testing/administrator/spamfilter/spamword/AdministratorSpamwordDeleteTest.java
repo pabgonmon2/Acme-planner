67e7c955c872ecc0dd156ff5b91a2b0d25356633
pabgonmon2 <pablogonmon@gmail.com>
2021-05-19 13:18:36 +0200

    Fixed delete spamword percentage

--- src/test/java/acme/testing/administrator/spamfilter/spamword/AdministratorSpamwordDeleteTest.java
+++ src/test/java/acme/testing/administrator/spamfilter/spamword/AdministratorSpamwordDeleteTest.java
@@ -2,24 +2,20 @@ package acme.testing.administrator.spamfilter.spamword;
 
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.AcmePlannerTest;
 
 public class AdministratorSpamwordDeleteTest extends AcmePlannerTest{
 
-	public static Integer i=0;
-	@ParameterizedTest
-	@CsvFileSource(resources = "/administrator/spamfilter/listSpamword.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Test
 	@Order(10)
-	public void deletePositive(final int recordIndex, final String spamword) {
+	public void deletePositive() {
 		this.signIn("administrator", "administrator");
 		
 		super.driver.get("http://localhost:8080/Acme-Planner/administrator/spamword/delete?id=66");
 		
-		super.checkInputBoxHasValue("spamword", spamword);
-		super.clickOnSubmitButton("delete");
+		super.checkInputBoxHasValue("spamword", "duro");
+		super.clickOnSubmitButton("Delete");
 		
 		super.checkSimplePath("/master/welcome");
 		super.driver.get("http://localhost:8080/Acme-Planner/administrator/spamword/show?id=66");
