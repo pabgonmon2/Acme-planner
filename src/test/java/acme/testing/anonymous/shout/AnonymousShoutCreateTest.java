e9e5994ce0321938c150350591b2cc20f98c07e6
pabgonmon2 <pablogonmon@gmail.com>
2021-05-19 11:28:07 +0200

    Saving changes

--- src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateTest.java
+++ src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateTest.java
@@ -1,6 +1,7 @@
 package acme.testing.anonymous.shout;
 
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -10,23 +11,47 @@ public class AnonymousShoutCreateTest extends AcmePlannerTest{
 
 	
 	@ParameterizedTest
-	@CsvFileSource(resources = "/anonymous/spamword/create.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(10)
-	public void createPositive(final int recordIndex, final String author, final String text, final String info, final String moment) {
+	@CsvFileSource(resources = "/anonymous/shout/create.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(35)
+	public void filtrosShoutPositive() {
 		
-		super.clickOnMenu("Anonymous", "Shout!");
-
+		super.clickOnMenu("Anonymous", "Shouts!");
 		
-//		super.checkSimplePath("/administrator/threshold/show");
+		super.checkInputBoxHasValue("author", "John Doe");
+		super.checkInputBoxHasValue("text", "Lorem ipsum!");
+		super.checkInputBoxHasValue("info", "http://example.org");
 		
-		super.checkInputBoxHasValue("spamword", "");
+		super.fillInputBoxIn("author", "Pablo");
+		super.fillInputBoxIn("text", "Hola como estais?");
+		super.fillInputBoxIn("info", "");
 		
-		super.fillInputBoxIn("spamword", spamword);
-		super.clickOnSubmitButton("Create");
+		super.clickOnSubmitButton("Shout!");
 		
-		super.clickOnMenu("Administrator", "Spam filter");
-		super.checkColumnHasValue(0, 0, spamword);
+		super.clickOnMenu("Anonymous", "List shout");
 		
-		this.signOut();
+		super.checkColumnHasValue(0, 1, "Pablo");
+		super.checkColumnHasValue(0, 2, "Hola como estais?");
+		super.checkColumnHasValue(0, 3, "");
+		
+	}
+	
+	@Test
+	@Order(40)
+	public void filtrosShoutNegative() {
+		
+		super.clickOnMenu("Anonymous", "Shouts!");
+		
+		super.checkInputBoxHasValue("author", "John Doe");
+		super.checkInputBoxHasValue("text", "Lorem ipsum!");
+		super.checkInputBoxHasValue("info", "http://example.org");
+		
+		super.fillInputBoxIn("author", "Sex");
+		super.fillInputBoxIn("text", "Hola como estais?");
+		super.fillInputBoxIn("info", "");
+		
+		super.clickOnSubmitButton("Shout!");
+		
+		super.checkErrorsExist();
+		
 	}
 }
