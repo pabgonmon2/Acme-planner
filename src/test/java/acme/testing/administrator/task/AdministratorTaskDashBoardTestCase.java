2672df4d1392332083779d2241fe462d9f80843b
carlo <carlo@DESKTOP-5JRTNIB>
2021-05-22 17:26:27 +0200

    añadidos casos de pruebas negativos

--- src/test/java/acme/testing/administrator/task/AdministratorTaskDashBoardTestCase.java
+++ src/test/java/acme/testing/administrator/task/AdministratorTaskDashBoardTestCase.java
@@ -1,12 +1,15 @@
 package acme.testing.administrator.task;
 
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.AcmePlannerTest;
 
 public class AdministratorTaskDashBoardTestCase extends AcmePlannerTest{
+	
+	/*En el siguiente test se provara que los valores numericos devueltos por el servicio sean correctos*/
 	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/administrator/task/task-dashboard.csv", encoding = "utf-8", numLinesToSkip = 1)
@@ -58,6 +61,23 @@ public class AdministratorTaskDashBoardTestCase extends AcmePlannerTest{
 		
 		this.signOut();
 	}
+	
+	/*En el siguiente test se provara la no posibilidad de acceder al dashboard por parte de un manager*/
+	@Test
+	@Order(20)
+	public void dashBoardNegativeTestCase2() {
+			this.signIn("manager3", "manager3");
+			super.driver.get("http://localhost:8080/Acme-Planner/administrator/dashboard/list");
+			super.checkErrorsExist();
+			this.signOut();
+	}
 
-
+	/*En el siguiente test se provara la no posibilidad de acceder al dashboard por parte de un anonimo*/
+	
+	@Test
+	@Order(30)
+	public void dashBoardNegativeTestCase3() {
+			super.driver.get("http://localhost:8080/Acme-Planner/administrator/dashboard/list");
+			super.checkErrorsExist();
+	}
 }
