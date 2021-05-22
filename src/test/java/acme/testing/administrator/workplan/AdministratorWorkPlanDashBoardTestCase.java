7ec1c0cfa9b9ae6131548b95e14b1c8e6d62350f
Carlos Villadiego <carvilgar1@alum.us.es>
2021-05-22 20:33:07 +0200

    Test workplan con casos negativos 100% funcionando

--- src/test/java/acme/testing/administrator/workplan/AdministratorWorkPlanDashBoardTestCase.java
+++ src/test/java/acme/testing/administrator/workplan/AdministratorWorkPlanDashBoardTestCase.java
@@ -1,6 +1,7 @@
 package acme.testing.administrator.workplan;
 
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -8,10 +9,12 @@ import acme.testing.AcmePlannerTest;
 
 public class AdministratorWorkPlanDashBoardTestCase extends AcmePlannerTest{
 	
+/*En el siguiente test se provara que los valores numericos devueltos por el servicio sean correctos*/
+	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/administrator/workplan/workplan-dashboard.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void listAll(final int recordIndex, 
+	public void listAll( 
 	final String publicTasks,
 	final String privateTasks,
 	final String finishedTasks,
@@ -28,36 +31,27 @@ public class AdministratorWorkPlanDashBoardTestCase extends AcmePlannerTest{
 		this.signIn("administrator", "administrator");
 		super.clickOnMenu("Administrator", "Dashboard Work Plans");
 		
-		super.checkColumnHasValue(recordIndex, 0, publicTasks);
-		super.checkColumnHasValue(recordIndex, 1, privateTasks);
-		super.checkColumnHasValue(recordIndex, 2, finishedTasks);
-		super.checkColumnHasValue(recordIndex, 3, nonFinishedTasks);
-		super.checkColumnHasValue(recordIndex, 4, averageWorkFlow);
-		super.checkColumnHasValue(recordIndex, 5, deviationWorkFlow);
-		super.checkColumnHasValue(recordIndex, 6, maxWorkFlow);
-		super.checkColumnHasValue(recordIndex, 7, minWorkFlow);
-		super.checkColumnHasValue(recordIndex, 8, averageExecutionPeriod);
-		super.checkColumnHasValue(recordIndex, 9, deviationExecutionPeriod);
-		super.checkColumnHasValue(recordIndex, 10, maxExecutionPeriod);
-		super.checkColumnHasValue(recordIndex, 11, minExecutionPeriod);
-		
-		super.clickOnListingRecord(recordIndex);
-		
-		super.checkInputBoxHasValue("publicTasks", publicTasks);
-		super.checkInputBoxHasValue("privateTasks", privateTasks);
-		super.checkInputBoxHasValue("finishedTasks", finishedTasks);
-		super.checkInputBoxHasValue("nonFinishedTasks", nonFinishedTasks);
-		super.checkInputBoxHasValue("averageWorkFlow", averageWorkFlow);
-		super.checkInputBoxHasValue("deviationWorkFlow", deviationWorkFlow);
-		super.checkInputBoxHasValue("maxWorkFlow", maxWorkFlow);
-		super.checkInputBoxHasValue("minWorkFlow", minWorkFlow);
-		super.checkInputBoxHasValue("averageExecutionPeriod", averageExecutionPeriod);
-		super.checkInputBoxHasValue("deviationExecutionPeriod", deviationExecutionPeriod);
-		super.checkInputBoxHasValue("maxExecutionPeriod", maxExecutionPeriod);
-		super.checkInputBoxHasValue("minExecutionPeriod", minExecutionPeriod);
+		super.checkDashBoard(publicTasks, privateTasks, finishedTasks, nonFinishedTasks, averageWorkFlow, deviationWorkFlow, maxWorkFlow, minWorkFlow, averageExecutionPeriod, deviationExecutionPeriod, maxExecutionPeriod, minExecutionPeriod);;
 		
 		this.signOut();
 	}
+	
+	/*En el siguiente test se provara la no posibilidad de acceder al dashboard por parte de un manager*/
+	@Test
+	@Order(20)
+	public void workPlanDashBoardNegativeTestCase2() {
+			this.signIn("manager3", "manager3");
+			super.driver.get("http://localhost:8080/Acme-Planner/administrator/dashboardwp/list");
+			super.checkErrorsExist();
+			this.signOut();
+	}
 
-
+	/*En el siguiente test se provara la no posibilidad de acceder al dashboard por parte de un anonimo*/
+	
+	@Test
+	@Order(30)
+	public void workPlanDashBoardNegativeTestCase3() {
+			super.driver.get("http://localhost:8080/Acme-Planner/administrator/dashboardwp/list");
+			super.checkErrorsExist();
+	}
 }
