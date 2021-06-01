2d245ec77c86ad82248bcf919ad0786fb848cb90
pabgonmon2 <pablogonmon@gmail.com>
2021-06-01 16:22:13 +0200

    commiting

--- src/test/java/acme/testing/authenticated/useraccount/AuthenticatedUserAccountUpdateTest.java
+++ src/test/java/acme/testing/authenticated/useraccount/AuthenticatedUserAccountUpdateTest.java
@@ -12,7 +12,7 @@ public class AuthenticatedUserAccountUpdateTest extends AcmePlannerTest{
 	@ParameterizedTest
     @CsvFileSource(resources = "/authenticated/useraccount/positive.csv", encoding = "utf-8", numLinesToSkip = 1)    
 	@Order(20)
-    public void updateUserAccountPositiveCase(
+    public void updatePositive(
     		final String password,
     		final String confirmation, 
     		final String name,
@@ -39,11 +39,11 @@ public class AuthenticatedUserAccountUpdateTest extends AcmePlannerTest{
     }
 
 	/*En este test verificaremos que no posible actualizar la 
-	  información de un usuario registrado debido a que las contraseñas no coinciden*/
+	  información de un usuario registrado debido a diferentes restricciones*/
 	@ParameterizedTest
     @CsvFileSource(resources = "/authenticated/useraccount/negative.csv", encoding = "utf-8", numLinesToSkip = 1)    
 	@Order(30)
-    public void updateUserAccountNegativeCase(
+    public void updateNegative(
     		final String password,
     		final String confirmation, 
     		final String name,
@@ -59,91 +59,91 @@ public class AuthenticatedUserAccountUpdateTest extends AcmePlannerTest{
         super.fillInputBoxIn("identity.email", email);
         
         super.clickOnSubmitButton("Update");
-        super.checkErrorsExist("confirmation");
+        super.checkErrorsExist();
 
         super.signOut();
     }
 
-	/*En este test verificaremos que no posible actualizar la 
-	  información de un usuario registrado debido a que los campos están vacios*/
-	@ParameterizedTest
-    @CsvFileSource(resources = "/authenticated/useraccount/negative2.csv", encoding = "utf-8", numLinesToSkip = 1)    
-	@Order(40)
-    public void updateUserAccountNegativeCase2(
-    		final String password,
-    		final String confirmation, 
-    		final String name,
-    		final String surname,
-    		final String email) {
-        super.signIn("manager2", "carvilgar1");
-        super.clickOnMenu("Account", "General data");
-        
-        super.fillInputBoxIn("password", password);
-        super.fillInputBoxIn("confirmation", confirmation);
-        super.fillInputBoxIn("identity.name", name);
-        super.fillInputBoxIn("identity.surname", surname);
-        super.fillInputBoxIn("identity.email", email);
-        
-        super.clickOnSubmitButton("Update");
-        
-        super.checkErrorsExist("password");
-        super.checkErrorsExist("identity.name");
-        super.checkErrorsExist("identity.surname");
-        super.checkErrorsExist("identity.email");
-
-        super.signOut();
-    }
-	
-	/*En este test verificaremos que no posible actualizar la 
-	  información de un usuario registrado debido a que las contraseñas son muy cortas*/
-	@ParameterizedTest
-	@CsvFileSource(resources = "/authenticated/useraccount/negative3.csv", encoding = "utf-8", numLinesToSkip = 1)    
-	@Order(50)
-	public void updateUserAccountNegativeCase3(
-			final String password,
-			final String confirmation, 
-			final String name,
-			final String surname,
-			final String email) {
-	    super.signIn("manager2", "carvilgar1");
-	    super.clickOnMenu("Account", "General data");
-	    
-	    super.fillInputBoxIn("password", password);
-	    super.fillInputBoxIn("confirmation", confirmation);
-	    super.fillInputBoxIn("identity.name", name);
-	    super.fillInputBoxIn("identity.surname", surname);
-	    super.fillInputBoxIn("identity.email", email);
-	    
-	    super.clickOnSubmitButton("Update");
-	    super.checkErrorsExist("password");
-	
-	    super.signOut();
-	}
-	
-	/*En este test verificaremos que no posible actualizar la 
-	  información de un usuario registrado debido a que las contraseñas son muy largas*/
-	@ParameterizedTest
-	@CsvFileSource(resources = "/authenticated/useraccount/negative4.csv", encoding = "utf-8", numLinesToSkip = 1)    
-	@Order(60)
-	public void updateUserAccountNegativeCase4(
-			final String password,
-			final String confirmation, 
-			final String name,
-			final String surname,
-			final String email) {
-	  super.signIn("manager2", "carvilgar1");
-	  super.clickOnMenu("Account", "General data");
-	  
-	  super.fillInputBoxIn("password", password);
-	  super.fillInputBoxIn("confirmation", confirmation);
-	  super.fillInputBoxIn("identity.name", name);
-	  super.fillInputBoxIn("identity.surname", surname);
-	  super.fillInputBoxIn("identity.email", email);
-	  
-	  super.clickOnSubmitButton("Update");
-	  super.checkErrorsExist("password");
-	
-	  super.signOut();
-	}
+//	/*En este test verificaremos que no posible actualizar la 
+//	  información de un usuario registrado debido a que los campos están vacios*/
+//	@ParameterizedTest
+//    @CsvFileSource(resources = "/authenticated/useraccount/negative2.csv", encoding = "utf-8", numLinesToSkip = 1)    
+//	@Order(40)
+//    public void updateUserAccountNegativeCase2(
+//    		final String password,
+//    		final String confirmation, 
+//    		final String name,
+//    		final String surname,
+//    		final String email) {
+//        super.signIn("manager2", "carvilgar1");
+//        super.clickOnMenu("Account", "General data");
+//        
+//        super.fillInputBoxIn("password", password);
+//        super.fillInputBoxIn("confirmation", confirmation);
+//        super.fillInputBoxIn("identity.name", name);
+//        super.fillInputBoxIn("identity.surname", surname);
+//        super.fillInputBoxIn("identity.email", email);
+//        
+//        super.clickOnSubmitButton("Update");
+//        
+//        super.checkErrorsExist("password");
+//        super.checkErrorsExist("identity.name");
+//        super.checkErrorsExist("identity.surname");
+//        super.checkErrorsExist("identity.email");
+//
+//        super.signOut();
+//    }
+//	
+//	/*En este test verificaremos que no posible actualizar la 
+//	  información de un usuario registrado debido a que las contraseñas son muy cortas*/
+//	@ParameterizedTest
+//	@CsvFileSource(resources = "/authenticated/useraccount/negative3.csv", encoding = "utf-8", numLinesToSkip = 1)    
+//	@Order(50)
+//	public void updateUserAccountNegativeCase3(
+//			final String password,
+//			final String confirmation, 
+//			final String name,
+//			final String surname,
+//			final String email) {
+//	    super.signIn("manager2", "carvilgar1");
+//	    super.clickOnMenu("Account", "General data");
+//	    
+//	    super.fillInputBoxIn("password", password);
+//	    super.fillInputBoxIn("confirmation", confirmation);
+//	    super.fillInputBoxIn("identity.name", name);
+//	    super.fillInputBoxIn("identity.surname", surname);
+//	    super.fillInputBoxIn("identity.email", email);
+//	    
+//	    super.clickOnSubmitButton("Update");
+//	    super.checkErrorsExist("password");
+//	
+//	    super.signOut();
+//	}
+//	
+//	/*En este test verificaremos que no posible actualizar la 
+//	  información de un usuario registrado debido a que las contraseñas son muy largas*/
+//	@ParameterizedTest
+//	@CsvFileSource(resources = "/authenticated/useraccount/negative4.csv", encoding = "utf-8", numLinesToSkip = 1)    
+//	@Order(60)
+//	public void updateUserAccountNegativeCase4(
+//			final String password,
+//			final String confirmation, 
+//			final String name,
+//			final String surname,
+//			final String email) {
+//	  super.signIn("manager2", "carvilgar1");
+//	  super.clickOnMenu("Account", "General data");
+//	  
+//	  super.fillInputBoxIn("password", password);
+//	  super.fillInputBoxIn("confirmation", confirmation);
+//	  super.fillInputBoxIn("identity.name", name);
+//	  super.fillInputBoxIn("identity.surname", surname);
+//	  super.fillInputBoxIn("identity.email", email);
+//	  
+//	  super.clickOnSubmitButton("Update");
+//	  super.checkErrorsExist("password");
+//	
+//	  super.signOut();
+//	}
 
 }
